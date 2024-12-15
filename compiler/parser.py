a7b1647413197d861030dc7357020b8cc8ecfd67
Gabriel Tavares Gargur <ggargur@id.uff.br>
2024-12-15 12:10:20 -0300

    fixed node making

--- compiler/parser.py
+++ compiler/parser.py
@@ -30,7 +30,7 @@ class Parser():
 
     def parse_MAIN(self):
         self.consume(TokenType.RESERVED, TokenType.CLASS)
-        class_name = Node(TokenType.ID, [self.consume(TokenType.ID)])
+        class_name = Node("identifier", [self.consume(TokenType.ID)])
         self.consume(TokenType.PUNCTUATION, TokenType.OPEN_BRACES)
         self.consume(TokenType.RESERVED, TokenType.PUBLIC)
         self.consume(TokenType.RESERVED, TokenType.STATIC)
@@ -40,7 +40,7 @@ class Parser():
         self.consume(TokenType.RESERVED, TokenType.STRING)
         self.consume(TokenType.PUNCTUATION, TokenType.OPEN_BRACKET)
         self.consume(TokenType.PUNCTUATION, TokenType.CLOSE_BRACKET)
-        parameter = Node(TokenType.ID, [self.consume(TokenType.ID)])
+        parameter = Node("identifier", [self.consume(TokenType.ID)])
         self.consume(TokenType.PUNCTUATION, TokenType.CLOSE_PARENTHESES)
         self.consume(TokenType.PUNCTUATION, TokenType.OPEN_BRACES)
         commands = []
@@ -52,11 +52,11 @@ class Parser():
 
     def parse_CLASSE(self):
         self.consume(TokenType.RESERVED, TokenType.CLASS)
-        class_name = Node(TokenType.ID, [self.consume(TokenType.ID)])
+        class_name = Node("identifier", [self.consume(TokenType.ID)])
         parent_class = None
         if self.get_token().value == TokenType.EXTENDS.value:
             self.consume(TokenType.RESERVED, TokenType.EXTENDS)
-            parent_class = Node(TokenType.ID, [self.consume(TokenType.ID)])
+            parent_class = Node("identifier", [self.consume(TokenType.ID)])
         self.consume(TokenType.PUNCTUATION, TokenType.OPEN_BRACES)
         variables = []
         while self.get_token().value not in [TokenType.PUBLIC.value, TokenType.CLOSE_BRACES.value]:
@@ -69,14 +69,14 @@ class Parser():
 
     def parse_VAR(self):
         tipo = self.parse_TIPO()
-        identifier = Node(TokenType.ID, [self.consume(TokenType.ID)])
+        identifier = Node("identifier", [self.consume(TokenType.ID)])
         self.consume(TokenType.PUNCTUATION, TokenType.SEMICOLON)
         return Node("VAR", [tipo, identifier])
 
     def parse_METODO(self):
         self.consume(TokenType.RESERVED, TokenType.PUBLIC)
         tipo = self.parse_TIPO()
-        identifier = Node(TokenType.ID, [self.consume(TokenType.ID)])
+        identifier = Node("identifier", [self.consume(TokenType.ID)])
         self.consume(TokenType.PUNCTUATION, TokenType.OPEN_PARENTHESES)
         params = self.parse_PARAMS() if self.get_token().value != TokenType.CLOSE_PARENTHESES.value else Node("PARAMS", [])
         self.consume(TokenType.PUNCTUATION, TokenType.CLOSE_PARENTHESES)
@@ -97,24 +97,24 @@ class Parser():
         params = []
         if self.get_token().token_type is not TokenType.PUNCTUATION or self.get_token().value != TokenType.CLOSE_PARENTHESES.value:
             params.append(self.parse_TIPO())
-            params.append(Node(TokenType.ID, [self.consume(TokenType.ID)]))
+            params.append(Node("identifier", [self.consume(TokenType.ID)]))
             while self.get_token().value == TokenType.COMMA.value:
                 self.consume(TokenType.PUNCTUATION, TokenType.COMMA)
                 params.append(self.parse_TIPO())
-                params.append(Node(TokenType.ID, [self.consume(TokenType.ID)]))
+                params.append(Node("identifier", [self.consume(TokenType.ID)]))
         return Node("PARAMS", params)
 
     def parse_TIPO(self):
         token = self.get_token()
         if token.token_type is TokenType.RESERVED and token.value in [TokenType.INT.value, TokenType.BOOL.value]:
-            tipo = Node(TokenType.RESERVED, [self.consume(TokenType.RESERVED)])
+            tipo = Node("reserved", [self.consume(TokenType.RESERVED)])
             if tipo.children[0].value == TokenType.INT.value and self.get_token().value == TokenType.OPEN_BRACKET.value:
                 self.consume(TokenType.PUNCTUATION, TokenType.OPEN_BRACKET)
                 self.consume(TokenType.PUNCTUATION, TokenType.CLOSE_BRACKET)
                 return Node("TIPO", [tipo, Node("ARRAY")])
             return Node("TIPO", [tipo])
         elif token.token_type is TokenType.ID:
-            identifier = Node(TokenType.ID, [self.consume(TokenType.ID)])
+            identifier = Node("identifier", [self.consume(TokenType.ID)])
             return Node("TIPO", [identifier])
         else:
             raise Exception(f"Expected type, got {repr(token)} @ {self.index}")
@@ -137,29 +137,29 @@ class Parser():
             if self.get_token() and self.get_token().value == TokenType.ELSE.value:
                 self.consume(TokenType.RESERVED, TokenType.ELSE)
                 else_cmd = self.parse_CMD()
-                return Node("CMD", [Node(TokenType.IF, [exp, cmd]), Node(TokenType.ELSE, [else_cmd])])
-            return Node("CMD", [Node(TokenType.IF, [exp, cmd])])
+                return Node("CMD", [Node("if", [exp, cmd]), Node("else", [else_cmd])])
+            return Node("CMD", [Node("if", [exp, cmd])])
         elif token.value == TokenType.WHILE.value:
             self.consume(TokenType.RESERVED, TokenType.WHILE)
             self.consume(TokenType.PUNCTUATION, TokenType.OPEN_PARENTHESES)
             exp = self.parse_EXP()
             self.consume(TokenType.PUNCTUATION, TokenType.CLOSE_PARENTHESES)
             cmd = self.parse_CMD()
-            return Node("CMD", [Node(TokenType.WHILE, [exp, cmd])])
+            return Node("CMD", [Node("while", [exp, cmd])])
         elif token.value == TokenType.PRINT.value:
             self.consume(TokenType.RESERVED, TokenType.PRINT)
             self.consume(TokenType.PUNCTUATION, TokenType.OPEN_PARENTHESES)
             exp = self.parse_EXP()
             self.consume(TokenType.PUNCTUATION, TokenType.CLOSE_PARENTHESES)
             self.consume(TokenType.PUNCTUATION, TokenType.SEMICOLON)
-            return Node("CMD", [Node(TokenType.PRINT, [exp])])
+            return Node("CMD", [Node("System.out.println", [exp])])
         elif token.token_type is TokenType.ID:
-            identifier = Node(TokenType.ID, [self.consume(TokenType.ID)])
+            identifier = Node("identifier", [self.consume(TokenType.ID)])
             if self.get_token().value == TokenType.EQUALS.value:
                 self.consume(TokenType.OP, TokenType.EQUALS)
                 exp = self.parse_EXP()
                 self.consume(TokenType.PUNCTUATION, TokenType.SEMICOLON)
-                return Node("CMD", [identifier, Node(TokenType.OP, TokenType.EQUALS), exp])
+                return Node("CMD", [identifier, Node("operator", "="), exp])
             elif self.get_token().value == TokenType.OPEN_BRACKET.value:
                 self.consume(TokenType.PUNCTUATION, TokenType.OPEN_BRACKET)
                 index_exp = self.parse_EXP()
@@ -167,7 +167,7 @@ class Parser():
                 self.consume(TokenType.OP, TokenType.EQUALS)
                 value_exp = self.parse_EXP()
                 self.consume(TokenType.PUNCTUATION, TokenType.SEMICOLON)
-                return Node("CMD", [identifier, index_exp, Node(TokenType.OP, TokenType.EQUALS), value_exp])
+                return Node("CMD", [identifier, index_exp, Node("operator", "="), value_exp])
         else:
             raise Exception(f"Expected command, got {repr(token)} @ {self.index}")
 
@@ -176,13 +176,13 @@ class Parser():
         while self.get_token() and self.get_token().value == TokenType.AND.value:
             self.consume(TokenType.OP, TokenType.AND)
             right = self.parse_REXP()
-            left = Node("EXP", [left, Node(TokenType.OP, TokenType.AND), right])
+            left = Node("EXP", [left, Node("operator", "&&"), right])
         return left
 
     def parse_REXP(self):
         left = self.parse_AEXP()
         while self.get_token() and self.get_token().value in [TokenType.LESS_THAN.value, TokenType.EQUALS_EQUALS.value, TokenType.DIFFERENT_THAN.value]:
-            operator = Node(TokenType.OP, [self.consume(TokenType.OP)])
+            operator = Node("operator", [self.consume(TokenType.OP)])
             right = self.parse_AEXP()
             left = Node("REXP", [left, operator, right])
         return left
@@ -190,7 +190,7 @@ class Parser():
     def parse_AEXP(self):
         left = self.parse_MEXP()
         while self.get_token() and self.get_token().value in [TokenType.PLUS.value, TokenType.MINUS.value]:
-            operator = Node(TokenType.OP, [self.consume(TokenType.OP)])
+            operator = Node("operator", [self.consume(TokenType.OP)])
             right = self.parse_MEXP()
             left = Node("AEXP", [left, operator, right])
         return left
@@ -198,7 +198,7 @@ class Parser():
     def parse_MEXP(self):
         left = self.parse_SEXP()
         while self.get_token() and self.get_token().value == TokenType.MULTIPLY.value:
-            operator = Node(TokenType.OP, [self.consume(TokenType.OP)])
+            operator = Node("operator", [self.consume(TokenType.OP)])
             right = self.parse_SEXP()
             left = Node("MEXP", [left, operator, right])
         return left
@@ -208,36 +208,36 @@ class Parser():
         if token.value == TokenType.NOT.value:
             self.consume(TokenType.OP, TokenType.NOT)
             sexp = self.parse_SEXP()
-            return Node("SEXP", [Node(TokenType.OP, TokenType.NOT), sexp])
+            return Node("SEXP", [Node("operator", "!"), sexp])
         elif token.value == TokenType.MINUS.value:
-            self.consume(TokenType.OP, TokenType.MINUS)
+            self.consume("TokenType.OP", TokenType.MINUS)
             sexp = self.parse_SEXP()
-            return Node("SEXP", [Node(TokenType.OP, TokenType.MINUS), sexp])
+            return Node("SEXP", [Node("operator", "-"), sexp])
         elif token.value == TokenType.TRUE.value:
             self.consume(TokenType.RESERVED, TokenType.TRUE)
-            return Node("SEXP", [Node(TokenType.RESERVED, TokenType.TRUE)])
+            return Node("SEXP", [Node("reserved", "true")])
         elif token.value == TokenType.FALSE.value:
             self.consume(TokenType.RESERVED, TokenType.FALSE)
-            return Node("SEXP", [Node(TokenType.RESERVED, TokenType.FALSE)])
+            return Node("SEXP", [Node("reserved", "false")])
         elif token.token_type.value == "num":
             num = Node("num", [self.consume("num")])
             return Node("SEXP", [num])
         elif token.value == TokenType.NULL.value:
             self.consume(TokenType.RESERVED, TokenType.NULL)
-            return Node("SEXP", [Node(TokenType.RESERVED, TokenType.NULL)])
+            return Node("SEXP", [Node("reserved", "null")])
         elif token.value == TokenType.NEW.value and self.tokens[self.index + 1].value == TokenType.INT.value:
             self.consume(TokenType.RESERVED, TokenType.NEW)
             self.consume(TokenType.RESERVED, TokenType.INT)
             self.consume(TokenType.PUNCTUATION, TokenType.OPEN_BRACKET)
             exp = self.parse_EXP()
             self.consume(TokenType.PUNCTUATION, TokenType.CLOSE_BRACKET)
-            return Node("SEXP", [Node(TokenType.RESERVED, TokenType.NEW), Node(TokenType.RESERVED, TokenType.INT), exp])
+            return Node("SEXP", [Node("reserved", "new"), Node("reserved", "int"), exp])
         else:
             pexp = self.parse_PEXP()
             if self.get_token() and self.get_token().value == TokenType.DOT.value and self.tokens[self.index + 1].value == TokenType.LENGHT.value:
                 self.consume(TokenType.PUNCTUATION, TokenType.DOT)
                 self.consume(TokenType.ID, TokenType.LENGHT)
-                return Node("SEXP", [pexp, Node(TokenType.ID, TokenType.LENGHT)])
+                return Node("SEXP", [pexp, Node("identifier", "length")])
             elif self.get_token() and self.get_token().value == TokenType.OPEN_BRACKET.value:
                 self.consume(TokenType.PUNCTUATION, TokenType.OPEN_BRACKET)
                 exp = self.parse_EXP()
@@ -249,7 +249,7 @@ class Parser():
         base = self.parse_BasePEXP()
         while self.get_token() and self.get_token().value == TokenType.DOT.value:
             self.consume(TokenType.PUNCTUATION, TokenType.DOT)
-            identifier = Node(TokenType.ID, [self.consume(TokenType.ID)])
+            identifier = Node("identifier", [self.consume(TokenType.ID)])
             if self.get_token() and self.get_token().value == TokenType.OPEN_PARENTHESES.value:
                 self.consume(TokenType.PUNCTUATION, TokenType.OPEN_PARENTHESES)
                 exps = self.parse_EXPS() if self.get_token().value != TokenType.CLOSE_PARENTHESES.value else Node("EXPS", [])
@@ -263,20 +263,20 @@ class Parser():
     def parse_BasePEXP(self):
         token = self.get_token()
         if token.token_type is TokenType.ID:
-            identifier = Node(TokenType.ID, [self.consume(TokenType.ID)])
+            identifier = Node("identifier", [self.consume(TokenType.ID)])
             return Node("BasePEXP", [identifier])
         elif token.token_type is TokenType.NUMBER:
-            number = Node(TokenType.NUMBER, [self.consume(TokenType.NUMBER)])
+            number = Node("number", [self.consume(TokenType.NUMBER)])
             return Node("BasePEXP", [number])
         elif token.token_type is TokenType.RESERVED and token.value == TokenType.THIS.value:
             self.consume(TokenType.RESERVED, TokenType.THIS)
-            return Node("BasePEXP", [Node(TokenType.RESERVED, TokenType.THIS)])
+            return Node("BasePEXP", [Node("reserved", "this")])
         elif token.token_type is TokenType.RESERVED and token.value == TokenType.NEW.value:
             self.consume(TokenType.RESERVED, TokenType.NEW)
-            identifier = Node(TokenType.ID, [self.consume(TokenType.ID)])
+            identifier = Node("identifier", [self.consume(TokenType.ID)])
             self.consume(TokenType.PUNCTUATION, TokenType.OPEN_PARENTHESES)
             self.consume(TokenType.PUNCTUATION, TokenType.CLOSE_PARENTHESES)
-            return Node("BasePEXP", [Node(TokenType.RESERVED, TokenType.NEW), identifier])
+            return Node("BasePEXP", [Node("reserved", "new"), identifier])
         elif token.token_type is TokenType.PUNCTUATION and token.value == TokenType.OPEN_PARENTHESES.value:
             self.consume(TokenType.PUNCTUATION, TokenType.OPEN_PARENTHESES)
             exp = self.parse_EXP()
